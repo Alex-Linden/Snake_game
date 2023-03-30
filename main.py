2dd7588efc9f558890fa2206f89b179b4fb09c05
Alex Linden <alextl2004@gmail.com>
2023-03-30 09:55:12 -0700

    add quit event

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
-import time
+from pygame.locals import *
 
 if __name__ == "__main__":
     pygame.init()
 
     surface = pygame.display.set_mode((500, 500))
-    surface.fill((255, 255, 255))
+    surface.fill((58, 13, 79))
     pygame.display.flip()
 
-    time.sleep(5)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                pass
+            elif event.type == QUIT:
+                running = False
