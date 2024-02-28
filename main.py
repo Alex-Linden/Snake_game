9d74ae8815e2ab4a45354c8668c4e2ce34b9cea0
Alex Linden <alextl2004@gmail.com>
2024-02-28 15:26:15 -0800

    add up, down, left, right moves

--- main.py
+++ main.py
@@ -10,9 +10,25 @@ class Snake:
         self.y = 100
 
     def draw(self):
-        self.surface.fill((58, 13, 79))
+        self.parent_screen.fill((58, 13, 79))
         self.parent_screen.blit(self.block, (self.x, self.y))
         pygame.display.flip()
+
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+
+    def move_right(self):
+        self.x += 10
+        self.draw()
+
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+
+    def move_down(self):
+        self.y += 10
+        self.draw()
 
 
 class Game:
@@ -23,12 +39,6 @@ class Game:
         self.snake = Snake(self.surface)
         self.snake.draw()
 
-        # self.block = pygame.image.load("resources/block.jpeg").convert()
-        # self.block_x, self.block_y = 100, 100
-        # self.surface.blit(self.block, (self.block_x, self.block_y))
-
-        # pygame.display.flip()
-
     def run(self):
         running = True
         while running:
@@ -37,17 +47,13 @@ class Game:
                     if event.key == K_ESCAPE:
                         running = False
                     if event.key == K_LEFT:
-                        self.block_x -= 10
-                        self.draw_block()
+                        self.snake.move_left()
                     if event.key == K_RIGHT:
-                        self.block_x += 10
-                        self.draw_block()
+                        self.snake.move_right()
                     if event.key == K_UP:
-                        self.block_y -= 10
-                        self.draw_block()
+                        self.snake.move_up()
                     if event.key == K_DOWN:
-                        self.block_y += 10
-                        self.draw_block()
+                        self.snake.move_down()
                 elif event.type == QUIT:
                     running = False
 
