7633b1c6a31e285fa70719943dcdd2e57d9a1e12
Alex Linden <alextl2004@gmail.com>
2024-03-01 16:29:31 -0800

    update snake movement to be constant in given direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import time
 
 
 class Snake:
@@ -8,6 +9,7 @@ class Snake:
         self.block = pygame.image.load("resources/block.jpeg").convert()
         self.x = 100
         self.y = 100
+        self.direction = "down"
 
     def draw(self):
         self.parent_screen.fill((58, 13, 79))
@@ -15,19 +17,26 @@ class Snake:
         pygame.display.flip()
 
     def move_left(self):
-        self.x -= 10
-        self.draw()
+        self.direction = "left"
 
     def move_right(self):
-        self.x += 10
-        self.draw()
+        self.direction = "right"
 
     def move_up(self):
-        self.y -= 10
-        self.draw()
+        self.direction = "up"
 
     def move_down(self):
-        self.y += 10
+        self.direction = "down"
+
+    def walk(self):
+        if self.direction == "left":
+            self.x -= 10
+        if self.direction == "right":
+            self.x += 10
+        if self.direction == "up":
+            self.y -= 10
+        if self.direction == "down":
+            self.y += 10
         self.draw()
 
 
@@ -60,6 +69,9 @@ class Game:
                         self.snake.move_down()
                 elif event.type == QUIT:
                     running = False
+
+            self.snake.walk()
+            time.sleep(0.2)
 
 
 if __name__ == "__main__":
